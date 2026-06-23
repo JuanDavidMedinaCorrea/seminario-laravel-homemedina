d60d04a2390365f74d6d42d7418e40cc174c66d9
JuanDavidMedinaCorrea <juandavidmedina51@gmail.com>
2026-06-23 16:26:12 -0500

    Bloque 5: POO Clases y Objetos

--- src/index.php
+++ src/index.php
@@ -1,20 +1,14 @@
 <?php
+require_once 'POO/Producto.php';
 
-$nombre = "Juan David Medina";
-$edad = 20;
-$ciudad = "Cali";
-$carrera = "Ingenieria de Sistemas";
+$prod1 = new Producto("Laptop Gamer", 1500, 0.19, "Electrónica");
+$prod2 = new Producto("Mouse Inalámbrico", 80, 0.19, "Oficina");
+$prod3 = new Producto("Monitor 4K", 1200, 0.19, "Electrónica");
 
-
-echo "<h1>Mi presentación</h1>";
-echo "<p>Hola, soy <strong>$nombre</strong>, tengo $edad años, vivo en $ciudad y estudio $carrera.</p>";
-
-
-echo '<p>Con comillas simples: $nombre no se interpreta</p>';
-
-
-$semestre = 5;
-$materiaFavorita = "Bases de Datos";
-echo "<p>Estoy en $semestre semestre y mi materia favorita es $materiaFavorita.</p>";
+echo "<h2>Catálogo POO</h2>";
+echo "<ul>";
+echo "<li>" . $prod1->getInfo() . "</li>";
+echo "<li>" . $prod2->getInfo() . "</li>";
+echo "<li>" . $prod3->getInfo() . "</li>";
+echo "</ul>";
 ?>
-
